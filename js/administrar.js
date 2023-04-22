711d109217227a22b61f5a552b32ae3b82192c27
unknown <candeviolam@gmail.com>
2023-04-22 14:52:58 -0300

    se terminó página de iniciar sesión y se comenzó la de administrar

---
+++ js/administrar.js
@@ -0,0 +1,38 @@
+const productos_key = "productos";
+const productos = JSON.parse(localStorage.getItem(productos_key));
+let htmlString = "";
+
+//                      el elemento sobre el cual nosotros estamos trabajando
+productos.forEach((element) => {});
+
+function CreateItem() {
+  return `<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+  </table>`;
+}
--- js/iniciar_sesion.js
+++ js/iniciar_sesion.js
@@ -3,15 +3,66 @@
 //                      puedo llamar un elemento por id (#), clase (.), o etiqueta (div)
 const mail = document.querySelector("#mail");
 const password = document.querySelector("#password");
+const btnLogin = document.getElementById("login");
+const btnClose = document.getElementById("close");
+const btnLogout = document.querySelector("#logout");
+const btnLoginPage = document.getElementById("login-page");
+const administrar = document.getElementById("administrar");
 
 let email = "";
 let pass = ""; // para poder almacenar los datos de abajo en una variable es que hago estas variables (valga la redundancia)
+
+const key = "sesion";
+
+const sesion = JSON.parse(localStorage.getItem(key));
+if (sesion) {
+  btnLoginPage.style.display = "none";
+  btnLogout.style.display = "block";
+  administrar.style.display = "block"; 
+}
+
+//como se rompió éste de abajo (no andaba el window.onload porque ya lo tengo en index.js, y tengo los dos scripts incrustado en el html, y el window.onload sólamente puede escuchar un evento (y éstos ya iban a ser dos)), hicimos el de arriba
+// window.onload = function () {
+//   const sesion = JSON.parse(localStorage.getItem(key));
+//   //   este "sesion" es el resumen de la lógica if(sesion !== null && sesion !== undefined) -> es decir, "si "sesion" existe" ...
+//   if (sesion) {
+//     btnLoginPage.style.display = "none";
+//     btnLogout.style.display = "block";
+//     administrar.style.display = "block";
+//   }
+// };
 
 mail.addEventListener("change", function (e) {
   email = e.target.value; // acá estoy almacenando los datos
 });
 
 password.addEventListener("change", function (e) {
-  pass = e.target.value;
+  pass = e.target.value; // capturando los valores y guardándolos en una variable
 });
 
+btnLogin.addEventListener("click", function () {
+  if (email === "admin" && pass === "admin") {
+    const usuario = new Usuario(email, pass, "admin", true);
+    localStorage.setItem(key, JSON.stringify(usuario));
+    btnClose.click();
+    btnLoginPage.style.display = "none";
+    btnLogout.style.display = "block";
+    administrar.style.display = "block";
+  } else {
+    console.error("Usuario o contraseña incorrectos");
+  }
+});
+
+class Usuario {
+  mail = "";
+  password = "";
+  rol = "";
+  isLogged = false;
+
+  constructor(mail, password, rol, isLogged) {
+    this.mail = mail;
+    this.password = password;
+    this.rol = rol;
+    this.isLogged = isLogged;
+  }
+}
